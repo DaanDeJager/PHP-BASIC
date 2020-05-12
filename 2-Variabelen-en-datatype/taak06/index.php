78b8281bf4ff4471a0df874c1ba21e0c8357b523
Daan de jager <daandejager2002@gmail.com>
2020-05-12 12:35:38 +0200

    tot 6

---
+++ 2-Variabelen-en-datatype/taak06/index.php
@@ -0,0 +1,14 @@
+<?php
+$string = "string";
+$integer = 69420;
+$float = 10.365;
+$boolean = true;
+$null = null;
+
+echo gettype($string);
+echo gettype($integer);
+echo gettype($float);
+echo gettype($boolean);
+echo gettype($null);
+
+?>
