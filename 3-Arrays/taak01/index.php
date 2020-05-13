6819f6200422f3fba11b3dfb71e138638a708bce
Daan de jager <daandejager2002@gmail.com>
2020-05-13 10:46:42 +0200

    3 af

---
+++ 3-Arrays/taak01/index.php
@@ -0,0 +1,4 @@
+<?php
+$myMovies = array("6Underground", "Joker", "Extraction");
+echo "<li>" . $myMovies[0] . "</li><li> " . $myMovies[1] . "</li><li>" . $myMovies[2] . "</li>";
+?>
---
+++ 3-Arrays/taak02/index.php
@@ -0,0 +1,7 @@
+<?php
+$myDogNames = array("Pieter" , "Jan" , "Hendrik");
+$myDogNames[1] = "Henk";
+echo $myDogNames[0];
+echo $myDogNames[1];
+echo $myDogNames[2];
+?>
